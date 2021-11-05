d2f9ef7c36771f20350d0643b65d302362e1e525
Dani <danillano97@gmail.com>
2021-11-04 23:26:36 -0300

    fixing bugs

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
 const formatMessage = require('./utils/messages');
-
 const {
     userJoin,
     getCurrentUser,
